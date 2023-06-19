f0fecc731289b54e0ca7f89eb651a61bd17603d2
aleksajaw <jaworska.aleksa@gmail.com>
2023-06-19 18:47:14 +0200

    Add view-subscribers_existing.php

--- inc/view-subscribers.php
+++ inc/view-subscribers.php
@@ -1,120 +1,33 @@
 
 
-        <?php
+<?php
 
-            require('./config/db.php');
-                        
-            require('./inc/view-subscribers_filter-nav.php');
+require('./config/db.php');
             
-            // here we have the version for the unchanged subscriber's name
-            $query = "SELECT subscribers.number, subscribers.fname, subscribers.email, audit_subscribers.action_performed, audit_subscribers.date_added 
-                        FROM audit_subscribers JOIN subscribers ON audit_subscribers.subscriber_name = subscribers.fname  ORDER BY number DESC";
+require('./inc/view-subscribers_filter-nav.php');
 
-            $stmt = $mysqli->prepare($query);
-            $stmt->execute();
-            $result = $stmt->get_result();
+?>
 
+
+<div class="user-list__view">
+<h2>
+    Wyświetl użytkowników
+</h2>
+    
+    <?php
+            
+            require( './inc/view-subscribers_existing.php');
+
+
+            $userListTableBuilder->render();
+
+            $mysqli->close();
         ?>
 
-        <div class="user-list__view">
-            <h2>
-                Wyświetl użytkowników
-            </h2>
-                
-                <?php
-                        
-                        require( './inc/classes/userListTableBuilder.php');
+        
+</div>
+<footer>
 
-
-                        $userListTableBuilder = new userListTableBuilder();
-
-
-                        //echo $userListTableBuilder->userListHeader(['#','Name','Email','Action Performed', 'Date', 'Action']);
-
-                        
-                        $headingsInfo = mysqli_fetch_fields($result);
-                        $headingsArr[] = 'user_action';
-
-                        foreach($headingsInfo as $headingInfo) {
-                            $headingName = $headingInfo->name;
-                            $headingsArr[] = $headingName;
-                        }
-                        $userListTableBuilder->userListHeader($headingsArr);
-                        
-                        $prevUserNumber = null;
-                        $nextRowsForSameUser = ['action_performed', 'date_added'];
-
-                        
-
-                        $actionClass = [
-                                'insert' => 'Insert a new subscriber',
-                                'update' => 'Updated a subscriber',
-                                'delete' => 'Deleted a subscriber'
-                            ];
-
-
-                        $rowClass = '';
-                        $noDataRowClass = '';
-
-                        
-                        if ($result->num_rows > 0) {
-
-                            while ($row = $result->fetch_assoc()) {
-                                                  
-                                $userNumber = isset($row['number']) ? $row['number'] : null;
-                                $hasUserNumber = $userNumber && $userNumber === $prevUserNumber;
-
-                                $isRowHidden = $hasUserNumber;
-                                $rowClass = ( $isRowHidden ) ? 'user-list__row--hidden-user ' : '';
-
-                                $userActionCell = $userListTableBuilder->userActionCell(['edit'=>$row["number"],'del'=>$row["number"]]);
-
-                                $userColumns = [];
-
-                                foreach($headingsInfo as $column) {
-                                    $colName = $column->name;
-                                    $userColumns[$colName] = $row[$colName];
-                                }
-
-                                $userRowCells = array_merge(
-                                                    [$userActionCell], 
-                                                    $userListTableBuilder->userDataCells($userColumns)
-                                                );
-
-
-                                if ( $row['action_performed'] != '') {
-                                    $actionClassKey = array_search($row['action_performed'], $actionClass);
-                                    $rowClass .= 'user-list__row--' . $actionClassKey;
-                                }
-
-                                $userListTableBuilder->userDataRow($userNumber, $userRowCells, $rowClass);
-
-                                $prevUserNumber = $userNumber;
-                            }
-                            $noDataRowClass = 'display-none';
-                        }
-                    
-                        $userListTableBuilder->noUserDataRow($noDataRowClass);
-                        
-                        
-                        $userActionCell = $userListTableBuilder->userActionCell(['add' => true]);
-                        $newUserCells = array_merge(
-                                            [$userActionCell],
-                                            $userListTableBuilder->newUserDataCells(null)
-                                        );
-
-                        $userListTableBuilder->userDataRow(null, $newUserCells, 'user-list__row--add-user') .
-
-
-                        $userListTableBuilder->render();
-
-                        $mysqli->close();
-                    ?>
-
-                    
-        </div>
-        <footer>
-            
-            <script src="./scripts/userListTable.js"></script>
-            <script src="./scripts/usersListFilter.js"></script>
-        </footer>
+<script src="./scripts/userListTable.js"></script>
+<script src="./scripts/usersListFilter.js"></script>
+</footer>
---
+++ inc/view-subscribers_existing.php
@@ -0,0 +1,107 @@
+
+
+<?php
+
+        $query = "SELECT audit_subscribers.subscriber_number AS number,
+                        audit_subscribers.subscriber_name AS fname, subscribers.email, audit_subscribers.action_performed,
+                        audit_subscribers.action_type, audit_subscribers.date_added
+                    FROM audit_subscribers JOIN subscribers ON audit_subscribers.subscriber_number = subscribers.number
+                    WHERE action_type IN ('insert', 'update')
+                    AND subscriber_number NOT IN (
+                        SELECT subscriber_number
+                        FROM audit_subscribers
+                        WHERE action_type = 'delete'
+                    )
+                    ORDER BY date_added DESC, number";
+
+        $stmt = $mysqli->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+
+    
+            require( './inc/classes/userListTableBuilder.php');
+
+
+            $userListTableBuilder = new userListTableBuilder();
+
+
+            //echo $userListTableBuilder->userListHeader(['#','Name','Email','Action Performed', 'Date', 'Action']);
+
+            $ignoredHeadings = ['action_type'];
+
+            $headingsInfo = mysqli_fetch_fields($result);
+            $headingsInfo = array_filter($headingsInfo, function($obj) use ($ignoredHeadings) {
+                return !in_array($obj->name, $ignoredHeadings);
+            });
+            $headingsArr[] = 'user_action';
+
+
+            foreach($headingsInfo as $headingInfo) {
+                $headingName = $headingInfo->name;
+                $headingsArr[] = $headingName;
+            }
+            $userListTableBuilder->userListHeader($headingsArr);
+            
+            $prevUserNumber = null;
+            $nextRowsForSameUser = ['action_performed', 'date_added'];
+            
+            $rowClass = '';
+            $noDataRowClass = '';
+
+            
+            if ($result->num_rows > 0) {
+
+                while ($row = $result->fetch_assoc()) {
+
+                    $userNumber = isset($row['number']) ? $row['number'] : null;
+                    $hasUserNumber = $userNumber && $userNumber === $prevUserNumber;
+                    
+                    $isRowHidden = $hasUserNumber;
+                    $rowClass = ( $isRowHidden ) ? 'user-list__row--hidden-user ' : '';
+                    
+                    $userActionCell = $userListTableBuilder->userActionCell(['edit'=>$userNumber,'del'=>$userNumber]);
+                    
+                    /*$userActionCell = ($row['number'] != $prevUserNumber)
+                                        ? $userListTableBuilder->userActionCell(['edit'=>$row["number"],'del'=>$row["number"]])
+                                        : $userListTableBuilder->userActionCell([]);*/
+
+                    $userColumns = [];
+
+                    foreach($headingsInfo as $column) {
+                        $colName = $column->name;
+                        $userColumns[$colName] = $row[$colName];
+                        /*$userColumns[] = ( $row['number'] != $prevUserNumber || in_array($colName, $nextRowsForSameUser) )
+                                            ? $row[$colName]
+                                            : '';*/
+                    }
+
+                    $userRowCells = array_merge(
+                                        [$userActionCell], 
+                                        $userListTableBuilder->userDataCells($userColumns)
+                                    );
+
+
+                    if ( isset($row['action_type']) ) $rowClass .= 'user-list__row--' . $row['action_type'];
+
+                    $userListTableBuilder->userDataRow($userNumber, $userRowCells, $rowClass);
+
+                    $prevUserNumber = $userNumber;
+                }
+                $noDataRowClass = 'display-none';
+            }
+
+
+            $userListTableBuilder->noUserDataRow($noDataRowClass);
+        
+
+            $userActionCell = $userListTableBuilder->userActionCell(['add' => true]);
+            $newUserCells = array_merge(
+                                [$userActionCell],
+                                $userListTableBuilder->newUserDataCells(null)
+                            );
+
+            $userListTableBuilder->userDataRow(null, $newUserCells, 'user-list__row--add-user');
+
+        ?>
+
